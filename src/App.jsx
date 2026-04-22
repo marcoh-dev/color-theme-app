37faea18c40cce0cd0a4905a1989449c55f20397
Marco <m.hebischoutlook.com>
2026-04-22 13:55:20 +0200

    added feature to delete a color

--- src/App.jsx
+++ src/App.jsx
@@ -12,15 +12,34 @@ function App() {
     setColors([{ id: uid(), role: formData.role, hex: formData.hex, contrastText: formData.contrastText }, ...colors]);
   }
 
+  function handleColorDelete(uid) {
+    setColors(colors.filter((color) => color.id !== uid));
+  }
+
   return (
     <>
       <h1>Theme Creator</h1>
       <ColorForm onSubmit={handleFormSubmit}></ColorForm>
       <ul className="color-themes">
         <li className="color-theme">
-          {colors.map((color) => (
-            <Color key={color.id} role={color.role} hex={color.hex} contrastText={color.contrastText} />
-          ))}
+          {colors.length == 0 ? (
+            <section className="no-color-card">
+              <p>
+                No colors... <strong>start by adding one!</strong>
+              </p>
+            </section>
+          ) : (
+            colors.map((color) => (
+              <Color
+                key={color.id}
+                id={color.id}
+                role={color.role}
+                hex={color.hex}
+                contrastText={color.contrastText}
+                onDelete={handleColorDelete}
+              />
+            ))
+          )}
         </li>
       </ul>
     </>
--- src/Components/Color/Color.jsx
+++ src/Components/Color/Color.jsx
@@ -1,11 +1,41 @@
 import "./Color.css";
+import { useState } from "react";
 
-export default function Color({ role, hex, contrastText }) {
+export default function Color({ id, role, hex, contrastText, onDelete }) {
+  const [hasClickedDelete, setHasClickedDelete] = useState(false);
+
+  function handleClickDelete() {
+    setHasClickedDelete(!hasClickedDelete);
+  }
+
+  function handleConfirmDelete() {
+    onDelete(id);
+  }
+
   return (
     <section className="color-card" style={{ backgroundColor: hex, color: contrastText }}>
-      <h2 className="color-card__headline">{hex}</h2>
+      <h2 className="color-card__highlight">{hex}</h2>
       <p className="color-card__role">{role}</p>
       <p className="color-card__contrast">contrast: {contrastText}</p>
+      <section className="color-card__buttons">
+        {hasClickedDelete ? (
+          <>
+            <div className="color-card__confirm">
+              <h3 className="color-card__highlight">Really delete?</h3>
+            </div>
+            <button className="color-card__button" onClick={handleClickDelete}>
+              Cancel
+            </button>
+            <button className="color-card__button" onClick={handleConfirmDelete}>
+              Delete
+            </button>
+          </>
+        ) : (
+          <button className="color-card__button" onClick={handleClickDelete}>
+            Delete
+          </button>
+        )}
+      </section>
     </section>
   );
 }
